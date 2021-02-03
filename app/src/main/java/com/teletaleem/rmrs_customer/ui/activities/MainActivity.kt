6a7f7ace94d6b438aec0ed68eaf44cd00d1eacae
Usama Wajid <usamawajid@teletaleem.com>
2021-02-03 17:06:13 +0500

    uploading project

---
+++ app/src/main/java/com/teletaleem/rmrs_customer/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.teletaleem.rmrs_customer.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teletaleem.rmrs_customer.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
