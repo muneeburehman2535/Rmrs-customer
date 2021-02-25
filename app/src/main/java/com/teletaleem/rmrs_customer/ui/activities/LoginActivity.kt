f384aa02e1b44b1caa66d4b96d6f2ff4f2a713f2
Usama Wajid <usamawajid@teletaleem.com>
2021-02-25 17:38:33 +0500

    Implement Login UI

--- app/src/main/java/com/teletaleem/rmrs_customer/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/teletaleem/rmrs_customer/ui/activities/LoginActivity.kt
@@ -1,12 +1,39 @@
 package com.teletaleem.rmrs_customer.ui.activities
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.teletaleem.rmrs_customer.R
+import com.teletaleem.rmrs_customer.databinding.ActivityLoginBinding
+import com.teletaleem.rmrs_customer.ui.view_models.LoginViewModel
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var mBinding:ActivityLoginBinding
+    private lateinit var mViewModel:LoginViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        //Bind Layout with data binding
+        mBinding=DataBindingUtil.setContentView(this,R.layout.activity_login)
+        mViewModel=ViewModelProvider(this).get(LoginViewModel::class.java)
+        mBinding.loginViewModel=mViewModel
+
+//        //Hide Toolbar
+//        @Suppress("DEPRECATION")
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//            window.insetsController?.hide(WindowInsets.Type.statusBars())
+//        } else {
+//            window.setFlags(
+//                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+//            )
+//        }
+
+
+
     }
 }
