333d17dd8ba2e6aeafe9af5922111bfed9d4d6cb
Muneeb Rehman <muneebrao661@gmail.com>
2021-08-13 03:36:06 -0700

    Implement Data binding in Restaurant Detail Screen

--- app/src/main/java/com/teletaleem/rmrs_customer/adapters/MenuAdapter.kt
+++ app/src/main/java/com/teletaleem/rmrs_customer/adapters/MenuAdapter.kt
@@ -10,9 +10,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.teletaleem.rmrs_customer.R
 import com.teletaleem.rmrs_customer.data_class.restaurantdetail.Menu
+import com.teletaleem.rmrs_customer.databinding.CardRestaurantDetailBinding
 import com.teletaleem.rmrs_customer.utilities.AppGlobal
 
 
@@ -23,15 +27,23 @@ class MenuAdapter(val requireContext: Context, private var menuList: ArrayList<M
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView=LayoutInflater.from(parent.context).inflate(R.layout.card_restaurant_detail, parent, false)
-        return ViewHolder(itemView)
+//        val itemView=LayoutInflater.from(parent.context).inflate(R.layout.card_restaurant_detail, parent, false)
+//        return ViewHolder(itemView)
+
+        val binding:CardRestaurantDetailBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.card_restaurant_detail,parent,false
+        )
+        return ViewHolder(binding)
     }
 
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        holder.txtMenuName.text=menuList[position].MenuName
-        holder.txtDescription.text=menuList[position].Description
+//        holder.txtMenuName.text=menuList[position].MenuName
+//        holder.txtDescription.text=menuList[position].Description
+
+        holder.bind(menuList[position])
         var calculatedPrice=0.0
         var itemPrice=0.0
         if (menuList[position].isVariant)
@@ -43,22 +55,22 @@ class MenuAdapter(val requireContext: Context, private var menuList: ArrayList<M
             calculatedPrice=menuList[position].CalculatedPrice.toDouble()
             itemPrice=menuList[position].ItemPrice.toDouble()
         }
-        holder.txtLatestPrice.text=AppGlobal.mCurrency+AppGlobal.roundTwoPlaces(calculatedPrice)
+        holder.binding.txtPriceCrd.text=AppGlobal.mCurrency+AppGlobal.roundTwoPlaces(calculatedPrice)
         if (calculatedPrice<itemPrice)
         {
             val oldPrice=AppGlobal.mCurrency+AppGlobal.roundTwoPlaces(itemPrice)
-            holder.txtOldPrice.visibility=View.VISIBLE
-            holder.txtOldPrice.setText(oldPrice, TextView.BufferType.SPANNABLE)
-            val spannable = holder.txtOldPrice.text as Spannable
+            holder.binding.txtOldPriceCrd.visibility=View.VISIBLE
+            holder.binding.txtOldPriceCrd.setText(oldPrice, TextView.BufferType.SPANNABLE)
+            val spannable = holder.binding.txtOldPriceCrd.text as Spannable
             spannable.setSpan(STRIKE_THROUGH_SPAN, 0, oldPrice.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
         else{
-            holder.txtOldPrice.visibility=View.GONE
+            holder.binding.txtOldPriceCrd.visibility=View.GONE
         }
 
-        holder.imgMenu.clipToOutline=true
+        holder.binding.imgMenuCrd.clipToOutline=true
         //AppGlobal.loadImageIntoGlide(menuList[position].Image,holder.imgMenu,requireContext)
-        AppGlobal.loadImageIntoPicasso(menuList[position].Image,holder.imgMenu)
+        AppGlobal.loadImageIntoPicasso(menuList[position].Image,holder.binding.imgMenuCrd)
 
     }
 
@@ -66,16 +78,39 @@ class MenuAdapter(val requireContext: Context, private var menuList: ArrayList<M
         return menuList.size
     }
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val txtMenuName =itemView.findViewById<TextView>(R.id.txt_menu_name_crd)!!
-        val txtDescription=itemView.findViewById<TextView>(R.id.txt_menu_crd)!!
-        val txtLatestPrice=itemView.findViewById<TextView>(R.id.txt_price_crd)!!
-        val txtOldPrice=itemView.findViewById<TextView>(R.id.txt_old_price_crd)!!
-        val imgMenu=itemView.findViewById<ImageView>(R.id.img_menu_crd)!!
+    class ViewHolder(var binding: CardRestaurantDetailBinding): RecyclerView.ViewHolder(binding.root) {
+//        val txtMenuName =itemView.findViewById<TextView>(R.id.txt_menu_name_crd)!!
+//        val txtDescription=itemView.findViewById<TextView>(R.id.txt_menu_crd)!!
+//        val txtLatestPrice=itemView.findViewById<TextView>(R.id.txt_price_crd)!!
+//        val txtOldPrice=itemView.findViewById<TextView>(R.id.txt_old_price_crd)!!
+//        val imgMenu=itemView.findViewById<ImageView>(R.id.img_menu_crd)!!
+
+        fun bind(data:Menu){
+
+            binding.restaurantDetailBinding = data
+            binding.executePendingBindings()
+        }
     }
 
     fun updateList(menuList: ArrayList<Menu>){
         this.menuList=menuList
         notifyDataSetChanged()
     }
+
+    companion object{
+        @JvmStatic
+        @BindingAdapter("loadDetailImage")
+        fun loadDetailImage(img_menu_crd:ImageView,url:String?){
+            if (!url.isNullOrEmpty()){
+                Glide.with(img_menu_crd.context).load(url).into(img_menu_crd)
+            }
+        }
+    }
+
+
+
+//    @JvmStatic
+//    @BindingAdapter(value = ["getDiscount", "getPrice"], requireAll = false)
+//    fun menuPrice(discount:)
+
 }
