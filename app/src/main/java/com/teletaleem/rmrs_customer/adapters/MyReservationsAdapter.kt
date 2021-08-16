cacd5b0bf78cad91b6c2212cfe8d2a672a569a92
Muneeb Rehman <muneebrao661@gmail.com>
2021-08-16 04:49:11 -0700

    DataBinding in Reservation Adapter

--- app/src/main/java/com/teletaleem/rmrs_customer/adapters/MyReservationsAdapter.kt
+++ app/src/main/java/com/teletaleem/rmrs_customer/adapters/MyReservationsAdapter.kt
@@ -9,9 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.teletaleem.rmrs_customer.R
 import com.teletaleem.rmrs_customer.data_class.reservation.Result
+import com.teletaleem.rmrs_customer.databinding.LayoutReservationBinding
 import com.teletaleem.rmrs_customer.utilities.AppGlobal
 import java.util.*
 import kotlin.collections.ArrayList
@@ -20,33 +22,42 @@ class MyReservationsAdapter(private val mContext:Context,private var reservation
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView=LayoutInflater.from(parent.context).inflate(R.layout.layout_reservation,parent,false)
-        return ViewHolder(itemView)
+//        val itemView=LayoutInflater.from(parent.context).inflate(R.layout.layout_reservation,parent,false)
+//        return ViewHolder(itemView)
+
+        val binding:LayoutReservationBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.layout_reservation,parent,false
+        )
+        return ViewHolder(binding)
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        holder.txtCustomerName.text=reservationList[position].CustomerName
-        holder.txtCustomerMobile.text=reservationList[position].MobileNumber
-        holder.txtRestaurantName.text=reservationList[position].RestaurantName
+//        holder.txtCustomerName.text=reservationList[position].CustomerName
+//        holder.txtCustomerMobile.text=reservationList[position].MobileNumber
+//        holder.txtRestaurantName.text=reservationList[position].RestaurantName
+
+        holder.bind(reservationList[position])
+
         when (reservationList[position].Status) {
             "AWAITING_ACCEPTANCE" -> {
-                holder.txtReservationStatus.text=mContext.getString(R.string.status_pending)
-                holder.txtReservationStatus.setTextColor(mContext.getColor(R.color.color_pending))
+                holder.binding.txtReservationStatusLr.text=mContext.getString(R.string.status_pending)
+                holder.binding.txtReservationStatusLr.setTextColor(mContext.getColor(R.color.color_pending))
             }
             "RESERVED" -> {
-                holder.txtReservationStatus.text=mContext.getString(R.string.title_reserved)
-                holder.txtReservationStatus.setTextColor(mContext.getColor(R.color.color_reserved))
+                holder.binding.txtReservationStatusLr.text=mContext.getString(R.string.title_reserved)
+                holder.binding.txtReservationStatusLr.setTextColor(mContext.getColor(R.color.color_reserved))
             }
             "REJECTED" -> {
-                holder.txtReservationStatus.text=mContext.getString(R.string.title_rejected)
-                holder.txtReservationStatus.setTextColor(mContext.getColor(R.color.color_rejected))
+                holder.binding.txtReservationStatusLr.text=mContext.getString(R.string.title_rejected)
+                holder.binding.txtReservationStatusLr.setTextColor(mContext.getColor(R.color.color_rejected))
             }
             else -> {
-                holder.txtReservationStatus.text=mContext.getString(R.string.title_confirmed)
-                holder.txtReservationStatus.setTextColor(mContext.getColor(R.color.colorAccent))
+                holder.binding.txtReservationStatusLr.text=mContext.getString(R.string.title_confirmed)
+                holder.binding.txtReservationStatusLr.setTextColor(mContext.getColor(R.color.colorAccent))
             }
         }
 
@@ -56,8 +67,8 @@ class MyReservationsAdapter(private val mContext:Context,private var reservation
         val month=date[1].toInt()-1
         val calendar=Calendar.getInstance()
         calendar.set(date[0].toInt(),month,date[2].toInt(),time[0].toInt(),time[1].toInt())
-        holder.txtDate.text=AppGlobal.dateToTimeStamp(Date(calendar.timeInMillis),"MMM dd, yyyy hh:mm a")
-        holder.txtNoOfPeople.text="${reservationList[position].NumberOfPeople} People"
+        holder.binding.txtDateLr.text=AppGlobal.dateToTimeStamp(Date(calendar.timeInMillis),"MMM dd, yyyy hh:mm a")
+        holder.binding.txtNoOfPeopleLr.text="${reservationList[position].NumberOfPeople} People"
 
     }
 
@@ -65,13 +76,21 @@ class MyReservationsAdapter(private val mContext:Context,private var reservation
         return reservationList.size
     }
 
-    class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
-        val txtCustomerName=itemView.findViewById<TextView>(R.id.txt_customer_name_lr)!!
-        val txtCustomerMobile=itemView.findViewById<TextView>(R.id.txt_mobile_number_lr)!!
-        val txtDate=itemView.findViewById<TextView>(R.id.txt_date_lr)!!
-        val txtRestaurantName=itemView.findViewById<TextView>(R.id.txt_restaurant_name_lr)!!
-        val txtReservationStatus=itemView.findViewById<TextView>(R.id.txt_reservation_status_lr)!!
-        val txtNoOfPeople=itemView.findViewById<TextView>(R.id.txt_no_of_people_lr)!!
+    class ViewHolder(val binding: LayoutReservationBinding):RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(data : Result){
+
+            binding.reservationBinding = data
+            binding.executePendingBindings()
+        }
+
+
+//        val txtCustomerName=itemView.findViewById<TextView>(R.id.txt_customer_name_lr)!!
+//        val txtCustomerMobile=itemView.findViewById<TextView>(R.id.txt_mobile_number_lr)!!
+//        val txtDate=itemView.findViewById<TextView>(R.id.txt_date_lr)!!
+//        val txtRestaurantName=itemView.findViewById<TextView>(R.id.txt_restaurant_name_lr)!!
+//        val txtReservationStatus=itemView.findViewById<TextView>(R.id.txt_reservation_status_lr)!!
+//        val txtNoOfPeople=itemView.findViewById<TextView>(R.id.txt_no_of_people_lr)!!
     }
 
     fun updateReservationList(reservationList:ArrayList<Result>){
